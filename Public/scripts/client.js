a18ae0b602ed038ef9464ced48090a37c6e03bd6
Lizzie Q <lquaidoo@gmail.com>
2022-03-13 13:55:54 -0500

    update

--- Public/scripts/client.js
+++ Public/scripts/client.js
@@ -10,17 +10,75 @@ $('button').on('click',function (event) {
 });
 $(`#formCalc`).on('submit', addCalculation)
 
-  (); //
-// !!!!!!    call above - here the server let ...](review Instructions) !!!!!!!!
+  reset (); //   So ask...I must move function reset... !!!!!
+// !!!!!!    call above - here the server let ...](review Instructions) !!!!!!!! ASK...
+
 }// end onReady function 
 
 function addCalculation(event){
     event.preventDefault();
 }
 
-let calculator = {
-    
-
-}
+let theCalculator = {
+    firstNum: $(`firstInput`).val();
+    secondNum: $(`secondInput`).val();
+    operators: operator,
+    total: 0;
+};
+console.log(theCalculator);
 
 // ajax goes here / function
+$.ajax({
+    method: 'POST',
+    url: '/theCalculator',
+    data: theCalculator,
+   //  Oops no host here???!!!!!! 
+}).then((response) => {
+    console.log('POST response', response);
+
+    reset ();
+}
+);
+let operator;
+function clearFields(){  // auto fill Not always helpful...
+    $('#firstInput').val('');
+    $('#secondInput').val('');
+}
+
+function reset(){  // 3  (Review these steps)
+    let ajaxOptions = {
+        method:"GET",
+        url:'/theCalculator',
+    };
+}
+function render (theCalculator){
+    // to the DOM  !!! ISSUES.......
+$(#answer).empty(); 
+for (let result of theCalculator){   
+$(`#answer`).append(`
+<li>
+    ${result.firstNum} ${result.operators}
+    ${result.secondNum} = ${result.total}
+
+</li>
+`);
+$(`#theResult`.text(result.total);
+}
+} // ask about option / need for div between 61-63
+
+function totalRender(total) {
+    $(`#Result`).text(`{$total}`);
+}
+
+function total(){
+    let ajax = {
+        method:"GET",
+        ulr: `/result`,
+    };
+
+    $ajax(ajax).then(response => {
+        console.log('We did it!? Ajax request ~', response);
+    totalRender(response);
+    });
+}
+
--- Server/server.js
+++ Server/server.js
@@ -1,2 +1,15 @@
 // get & post in server
+const express = require('express');
+const app = express();
+app.use(express.static('server/public'))
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Array - hold the data 
+
+let calculation = []; // Wasn't   - take a break return!!!
+let Total =0;
+
+app.post('/calculator')
 
